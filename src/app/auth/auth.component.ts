850c5c0c9358af0f366f202c08a9d9f4c726d8af
Bojan Petrović <bojankg85@gmail.com>
2023-06-29 14:01:29 +0200

    commit

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit {
+  logIn: FormGroup;
 
+  ngOnInit() {
+    this.logIn = new FormGroup({
+      userData: new FormGroup({
+        user: new FormControl(null, [
+          Validators.required
+        ]),
+        password: new FormControl(null, [
+          Validators.required
+        ]),
+      })
+    });
+  }
+
+  log() {
+    let user = this.logIn.get('userData.user')?.value;
+    let password = this.logIn.get('userData.password')?.value;
+    console.log(user, "-", password)
+  }
 }
