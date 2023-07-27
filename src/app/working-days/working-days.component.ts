d74a69d8d37d56df8e4f5a5f3578dac40ed41110
Bojan Petrović <bojankg85@gmail.com>
2023-07-27 14:52:51 +0200

    commit

--- src/app/working-days/working-days.component.ts
+++ src/app/working-days/working-days.component.ts
@@ -4,6 +4,11 @@ import { WorkingDays } from '../workingDays';
 import { DetailService } from '../services/detail.service';
 import { EmployeeService } from '../services/employee.service';
 
+interface Month {
+  name: string;
+  code: number
+}
+
 @Component({
   selector: 'app-working-days',
   templateUrl: './working-days.component.html',
@@ -13,6 +18,9 @@ export class WorkingDaysComponent implements OnInit {
   workingDays: WorkingDays[] = [];
   employee: Employee;
   date: number;
+  month: Month[];
+  selectedMonth: Month | undefined;
+
 
   constructor(private detailService: DetailService, private employeeService: EmployeeService) {}
 
@@ -20,10 +28,42 @@ export class WorkingDaysComponent implements OnInit {
     this.detailService.getDetail().subscribe((data) => {
       this.employee = data;
       this.workingDays = Object.values(data.workingDays!);
-      console.log(data.workingDays!, this.employee);
     });
 
     const date = new Date;
     this.date = date.getTime();
+
+    this.month = [
+      {name: 'All', code: 12},
+      {name: 'January', code: 0},
+      {name: 'February', code: 1},
+      {name: 'March', code: 2},
+      {name: 'April', code: 3},
+      {name: 'May', code: 4},
+      {name: 'June', code: 5},
+      {name: 'July', code: 6},
+      {name: 'August', code: 7},
+      {name: 'September', code: 8},
+      {name: 'October', code: 9},
+      {name: 'November', code: 10},
+      {name: 'December', code: 11},
+    ]
+
+    this.selectedMonth = {name: 'All', code: 12};
+    console.log(this.selectedMonth)
+  }
+
+  getMonth() {
+    console.log(this.selectedMonth?.name);
+  }
+
+  dateCompar(dateCalck: number): boolean {
+    let date = new Date(dateCalck);
+    let selected = this.month.find(month => month.code === date.getMonth());
+
+
+    let com1 = selected?.name;
+    let com2 = this.selectedMonth?.name;
+    return com1 === com2;
   }
 }
